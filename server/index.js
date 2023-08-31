71dedaf882966227501aa393159f15ba52bd1dfb
BitHero <nuhumusah874@gmail.com>
2023-08-31 15:21:32 +0000

    routes, controllers

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import morgan from 'morgan'
 import helmet, { crossOriginResourcePolicy } from 'helmet'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import authRoutes from './routes/auth.js'
+import userRoutes from './routes/users.js'
 import { register } from './controllers/auth.js'
 
 
@@ -38,6 +40,10 @@ const storage = multer.diskStorage({
 const upload = multer({ storage })
 
 app.post("/auth/register", upload.single('picture'), register)
+
+// Routes
+app.use('/auth', authRoutes)
+app.use('/users', userRoutes)
 
 // mongoose setup
 const PORT = process.env.PORT || 6001
